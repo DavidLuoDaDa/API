b2137d2158c58a5701b991066b882af68a5e16ab
David <hnn1438@gmail.com>
2017-07-18 18:16:29 +0800

    cheange beep.js

--- beep.js
+++ beep.js
@@ -1,11 +1,23 @@
+//var io = require('socket.io')({
+//transports: ['websocket'],
+//});
+//
+//io.attach(4567);
+//
+//io.on('connection', function(socket){
+//socket.on('beep', function(){
+//socket.emit('boop');
+//});
+//})
+
 var io = require('socket.io')({
-	transports: ['websocket'],
+    transports: ['websocket'],
 });
 
 io.attach(4567);
 
-io.on('connection', function(socket){
-	socket.on('beep', function(){
-		socket.emit('boop');
-	});
+io.sockets.on('connection', function(socket) {
+    socket.on('beep', function() {
+        socket.emit('boop');
+    });
 })
