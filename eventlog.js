c6007496e67258c0adc60570e2c921d488686046
David <hnn1438@gmail.com>
2018-04-14 02:06:32 +0800

    update battle.js

---
+++ eventlog.js
@@ -0,0 +1,29 @@
+//http for mongodb
+var express = require('express');
+var bodyParser = require('body-Parser');
+var http = require('http');
+var app = express();
+app.use(bodyParser.json());
+
+//var port = process.env.port || 8000;
+http.createServer(app).listen(8000);
+
+//mongodb
+var MongoClient = require('mongodb').MongoClient;
+var Url = 'mongodb://admin:1qazQAZ3edcEDC@220.133.51.181:27017/admin';
+
+app.post('/Event', function(req, res) {
+    var InputData = req.body;
+
+    MongoClient.connect(Url, function(err, client) {
+        var db = client.db('Activity');
+        var col = db.collection('Event');
+        col.insert(InputData, function(err, result) {
+            if (err) {
+                client.close();
+            } else {
+                client.close();
+            }
+        });
+    });
+});
