193c038aac54e5058420938f448b51e853703de0
David <hnn1438@gmail.com>
2018-04-14 02:13:40 +0800

    update battle.js

--- battle.js
+++ battle.js
@@ -1,40 +1,8 @@
 const uuidv1 = require('uuid/v1');
-var server = require('http').createServer().listen(9000);
-
-//http for mongodb
-var express = require('express');
-var bodyParser = require('body-Parser');
-var http = require('http');
-var app = express();
-app.use(bodyParser.json());
-
-//var port = process.env.port || 8000;
-http.createServer(app).listen(8000);
-
-//mongodb
-var MongoClient = require('mongodb').MongoClient;
-var Url = 'mongodb://admin:1qazQAZ3edcEDC@220.133.51.181:27017/admin';
-
-app.post('/Event', function(req, res) {
-    var InputData = req.body;
-
-    MongoClient.connect(Url, function(err, client) {
-        var db = client.db('Activity');
-        var col = db.collection('Event');
-        col.insert(InputData, function(err, result) {
-            if (err) {
-                client.close();
-            } else {
-                client.close();
-            }
-        });
-    });
-});
 
 //socket.io
-
 //var io = require('socket.io')(server);
-
+var server = require('http').createServer().listen(9000);
 var io = require('socket.io')(server, {
     "serveClient": false,
     "transports": ['websocket', 'polling']
